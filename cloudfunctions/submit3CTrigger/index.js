aa3aa90d6a30accc36d34e88c642fb1a64583e0e
Fu-XDU <fuming.137@icloud.com>
2022-01-01 13:03:27 +0800

    🐛修复晨午晚检无法提交的BUG

--- cloudfunctions/submit3CTrigger/index.js
+++ cloudfunctions/submit3CTrigger/index.js
@@ -16,7 +16,7 @@ exports.main = async (event, context) => {
   // 承载所有读操作的 promise 的数组
   const tasks = []
   for (let i = 0; i < batchTimes; i++) {
-    const promise = db.collection('data').skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+    const promise = db.collection('3CheckData').skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
     tasks.push(promise)
   }
   // 等待所有
